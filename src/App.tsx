54a677f3d8dbde98d0361cfbda358d734b0347b6
MerciMarcy <masashi.krt74000011@gmail.com>
2022-06-21 01:57:40 +0900

    covid_dashboard完成

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+import DashBoard from './features/covid/Dashboard/DashBoard';
+
+
+function App() {
+  return (
+    <DashBoard />
+  );
+}
+
+export default App;
